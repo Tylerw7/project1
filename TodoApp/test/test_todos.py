34f2f263eaab594a40df4d1f8dbbddf7746e095e
Tylerw7 <t4wettach@gmail.com>
2025-05-24 11:45:06 -0400

    added unit testing

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -72,3 +72,44 @@ def test_read_all_authenticated(test_todo):
         "owner_id": 1,
 
     }]
+
+
+#Read on todo test
+def test_read_one_authenticated(test_todo):
+    response = client.get("/todo/1")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "complete": False, 
+        "title": "learn to code!", 
+        "description": "Need to learn everything",
+        "id": 1,
+        "priority": 5,
+        "owner_id": 1,
+
+    }   
+
+#Test if todo does not exist!
+def test_read_one_authenticated_not_found():
+         response = client.get('/todo/999')
+         assert response.status_code == 404
+         assert response.json() == {'detail': 'Todo not found!'}
+
+
+
+#------test creating a new todo-------
+def test_create_todo(test_todo):
+     request_data={
+          'title': 'New todo!',
+          'description': 'New todo description',
+          'priority': 5,
+          'complete': False,
+     }         
+
+     response = client.post('/todo/', json=request_data)
+     assert response.status_code == 201
+     db = TestingSessionLocal()
+     model = db.query(Todos).filter(Todos.id == 2).first()
+     assert model.title == request_data.get('title')
+     assert model.description == request_data.get('description')
+     assert model.priority == request_data.get('priority')
+     assert model.complete == request_data.get('complete')
