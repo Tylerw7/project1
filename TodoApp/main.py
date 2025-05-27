4d628821590b6aeb412da42ba8d427a9004ea99c
Tylerw7 <t4wettach@gmail.com>
2025-05-27 18:17:46 -0400

    started html

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,12 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import models
 from database import engine
 from routers import auth, todos, admin, users
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+BASE_DIR = Path(__file__).resolve().parent 
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+@app.get('/')
+def test(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 
 #Health check
