27d8c23ee3a9d4be6d2e363d96792279b3f0ae3b
Tylerw7 <t4wettach@gmail.com>
2025-05-28 15:26:02 -0400

    reverted back to sqlite3

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base #used to be sqlalchemy.ext.declarative
-SQLALCHEMY_DATABASE_URL='postgresql://postgres:test1234@localhost/TodoApplicationDatabase'
+#SQLALCHEMY_DATABASE_URL='postgresql://postgres:test1234@localhost/TodoApplicationDatabase'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+
+#engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- TodoApp/main.py
+++ TodoApp/main.py
@@ -4,6 +4,7 @@ from database import engine
 from routers import auth, todos, admin, users
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
@@ -13,6 +14,9 @@ models.Base.metadata.create_all(bind=engine)
 
 BASE_DIR = Path(__file__).resolve().parent 
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
+
 
 @app.get('/')
 def test(request: Request):
